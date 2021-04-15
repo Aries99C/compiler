1dfd64e00fcb73ab7dfa70b1d928b09503aba0d8
Aries99C <287767242@qq.com>
2021-04-16 00:46:01 +0800

    update json.

--- src/main/java/lexical/test.c
+++ src/main/java/lexical/test.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // comment
-    /* comment */
-    /*
-     * comment
-     */
-    /*
-     * "string"
-     */
-    int a /*
-    * comment */
-            = 10;
-    /* comment
-    */ int b = 12;
-    char c = '/*';
-    char d = '*/';
-    printf("%d", a);
-    printf("%d", b);
-    printf("hello//comment");
-    printf("/*comment*/");
-    printf("Hello, World!\n");
-    printf("%c", c);
-    printf("%c", d);
+    int a = 10;
+    int b = 12;
     return 0;
 }
