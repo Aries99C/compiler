f8629a3aefe967fab12df09311835ea167f959cf
Aries99C <287767242@qq.com>
2021-05-13 23:57:32 +0800

    lab 2 finished, demo is ok. report is needed.

--- src/main/java/grammatical/Production.java
+++ src/main/java/grammatical/Production.java
@@ -1,11 +1,43 @@
 package grammatical;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Production {
+    public int index = 0;
     public String left;
     public String[] rights;
+    public String hoping = "";
 
     public Production(String left, String[] rights) {
         this.left = left;
         this.rights = rights;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(left).append(" ::= ");
+        for (String right : rights) {
+            if (!right.equals("epsilon")) {
+                builder.append(right).append(" ");
+            }
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Production that = (Production) o;
+        return Objects.equals(left, that.left) && Arrays.equals(rights, that.rights);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(left);
+        result = 31 * result + Arrays.hashCode(rights);
+        return result;
     }
 }
